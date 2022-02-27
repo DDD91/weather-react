63b30a290c05bf3c6973b5973ccfc8367e7f5594
DDD91 <ghecci1614@gmail.com>
2022-02-27 10:32:53 -0500

    Added github footer

--- src/App.js
+++ src/App.js
@@ -11,6 +11,12 @@ function App() {
         </h1>
         <Weather  city="" />
        </header>
+       <footer>
+         <p className="text-center mt-5">
+"This project was coded by Devanie Dawson, and it is "
+<a href="https://github.com/DDD91/weather-react" target="_blank">open-sourced</a> 😊
+         </p>
+       </footer>
     </div>
   );
 }
